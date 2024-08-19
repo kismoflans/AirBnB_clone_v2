04a16b593774b5fa902be05d0508a20e9c2261b0
Amos Kiplimo <168921899+kismoflans@users.noreply.github.com>
2024-08-19 13:32:51 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""
-0x00. AirBnB clone - The console
-Review module
-"""
-from models.base_model import BaseModel
+"""This is the review class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
     """
-    Defines attributes/methods for the Review class, subclass of BaseModel
-    Other attributes/methods are inherited from BaseModel
-    """
-
-    place_id = ""
-    user_id = ""
-    text = ""
-
-    # def __init__(self, *args, **kwargs):
-    #     """initialize variables and methods"""
-    #     super().__init__(self, *args, **kwargs)
+    __tablename__ = "reviews"
+    text = Column(String(1024),
+                  nullable=False)
+    place_id = Column(String(60),
+                      ForeignKey("places.id", ondelete="CASCADE"),
+                      nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey("users.id", ondelete="CASCADE"),
+                     nullable=False)
